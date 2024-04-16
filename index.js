448f3d1bc9fde410ea02dae9af13a9287099f9b0
Ashirvad Kumar Pandey <116254270+Ashirvad-Kumar-Pandey-1-618@users.noreply.github.com>
2024-04-16 15:48:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
+const fs = require('fs').promises;
+const fetch = require('node-fetch');
+
 async function updateReadme() {
-    const fs = require('fs')
     let prevData = "";
 
-    fs.readFile('quotes.txt', (err, data) => {
-        if (err) throw err;
-        prevData += data.toString();
-    });
+    try {
+        prevData = await fs.readFile('quotes.txt', 'utf-8');
+    } catch (err) {
+        if (err.code !== 'ENOENT') throw err; // Only throw error if file not found
+    }
     
     let readme = "";
     
     try {
         const response = await fetch("https://api.quotable.io/random");
-        const data = await response.json();
+        if (!response.ok) throw new Error('response not OK');
 
-        readme = `Quote on ${new Date().toUTCString()}: `;
-    
-        if (response.ok) {
-            readme += ` ${data["content"]} - ${data["author"]}`;
-        } else {
-            throw new Error('response not OK');
-        }
+        const data = await response.json();
+        readme = `Quote on ${new Date().toUTCString()}: ${data.content} - ${data.author}`;
     } catch (e) {
-        readme += `Fetching failed`;
+        readme = `Fetching failed: ${e.message}`;
     }
     
     prevData += "\n";
